df16ce32d0f224a496259ad9440ce19c36d1fbee
Aitor Mendaza <aitorthered@gmail.com>
2020-05-06 14:21:27 -0400

    Added tests for makeOrderedLeafList

--- src/test/scala/patmat/HuffmanSuite.scala
+++ src/test/scala/patmat/HuffmanSuite.scala
@@ -7,12 +7,6 @@ class HuffmanSuite {
 
   import Huffman._
 
-  trait TestTrees {
-    val t1 = Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5)
-    val t2 = Fork(Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5), Leaf('d', 4), List('a', 'b', 'd'), 9)
-  }
-
-
   @Test def `weight of a larger tree (10pts)`: Unit =
     new TestTrees {
       assertEquals(5, weight(t1))
@@ -64,22 +58,29 @@ class HuffmanSuite {
   @Test def `string2chars hello world`: Unit =
     assertEquals(List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'), string2Chars("hello, world"))
 
-
   @Test def `make ordered leaf list for some frequency table (15pts)`: Unit =
     assertEquals(List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 3)), makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))))
 
+  @Test def `make ordered leaf list empty`: Unit =
+    assertEquals(List(), makeOrderedLeafList(List()))
+
+  @Test def `make ordered leaf list for one element`: Unit =
+    assertEquals(List(Leaf('x', 3)), makeOrderedLeafList(List(('x', 3))))
 
   @Test def `combine of some leaf list (15pts)`: Unit = {
     val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
     assertEquals(List(Fork(Leaf('e', 1), Leaf('t', 2), List('e', 't'), 3), Leaf('x', 4)), combine(leaflist))
   }
 
-
   @Test def `decode and encode a very short text should be identity (10pts)`: Unit =
     new TestTrees {
       assertEquals("ab".toList, decode(t1, encode(t1)("ab".toList)))
     }
 
-
   @Rule def individualTestTimeout = new org.junit.rules.Timeout(10 * 1000)
+
+  trait TestTrees {
+    val t1 = Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5)
+    val t2 = Fork(Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5), Leaf('d', 4), List('a', 'b', 'd'), 9)
+  }
 }
